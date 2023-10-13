062e7a2d8dd5e6a7522ba6e0faad51945057b6d0
shljsl75891 <shljsl75891@gmail.com>
2023-10-13 23:19:40 +0530

    nested and sibling elements using React.createElemet

--- 01_Inception/script.js
+++ 01_Inception/script.js
@@ -1,10 +1,28 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React !",
-);
+/*  
+  <div id="parent"> 
+    <div id="child_1">
+      <h1>I'm h1 tag</h1>
+      <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child_2">
+      <h1>I'm h1 tag</h1>
+      <h2>I'm h2 tag</h2>
+    </div>
+  </div>
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child_1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child_2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-root.render(heading);
+root.render(parent);
