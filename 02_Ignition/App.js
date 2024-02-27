87e85ed3215405aad566456ec31b854a4d782694
shljsl75891 <shljsl75891@gmail.com>
2024-02-27 20:28:50 +0530

    feat(bundler): give muscles to app using parcel

---
+++ 02_Ignition/App.js
@@ -0,0 +1,31 @@
+/*
+  <div id="parent">
+    <div id="child_1">
+      <h1>I'm h1 tag</h1>
+      <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child_2">
+      <h1>I'm h1 tag</h1>
+      <h2>I'm h2 tag</h2>
+    </div>
+  </div>
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child_1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child_2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(parent);
