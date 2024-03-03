f5e5559eff5f34df526e6ebe7c47d206a8c51065
shljsl75891 <shljsl75891@gmail.com>
2024-03-03 20:42:53 +0530

    feat(foundation): using JSX

--- 03_Foundation/App.js
+++ 03_Foundation/App.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 
 const mainContainer = document.getElementById("root");
 
+/*
 const container = React.createElement("div", { className: "title" }, [
   React.createElement("h1", {}, "Heading1"),
   React.createElement("h2", {}, "Heading2"),
   React.createElement("h3", {}, "Heading3"),
-]);
+]); */
+
+const container = (
+  <div className="title">
+    <h1>Heading1</h1>
+    <h2>Heading2</h2>
+    <h3>Heading3</h3>
+  </div>
+);
 
 const root = ReactDOM.createRoot(mainContainer);
 
